2166c21a63907440ce17992c6cf506b5c8e755b4
SimonKihara <kimcode254@gmail.com>
2020-04-11 02:10:00 +0300

    Second commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+const data = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+   }
+   
+const covid19ImpactEstimator = (data) => {
+    var currentlyInfected;
+    const impact = () => {
+        const currentlyInfected = data.reportedCases * 10;
+
+    }
+
+    const severeImpact = () => {
+        const currentlyInfected = data.reportedCases * 50;
+    }
+}
+    
 
 export default covid19ImpactEstimator;
