14943a760b55eeb3140b46a7060565554234ba68
SimonKihara <kimcode254@gmail.com>
2020-04-13 16:19:57 +0300

    g

--- src/estimator.js
+++ src/estimator.js
@@ -19,20 +19,18 @@ const currentlyInfected2 = data.reportedCases * 50;
 
 // Function to output the date entry mode
 function periodTypeMode() {
-  let dateMode;
+  let dateMode = 'days';
   if (data.periodType === 'months') {
     dateMode = Math.trunc((data.timeToElapse * 30) / 3);
   } else if (data.periodType === 'weeks') {
     dateMode = Math.trunc((data.timeToElapse * 7) / 3);
-  } else if (data.periodType === 'days') {
-    dateMode = Math.trunc(data.timeToElapse / 3);
   }
   return dateMode;
 }
 
 // Calculation for infected by the requested time
-const calc1 = Math.trunc(currentlyInfected1 * (2 ** periodTypeMode.dateMode));
-const calc2 = Math.trunc(currentlyInfected2 * (2 ** periodTypeMode.dateMode));
+const calc1 = Math.trunc(currentlyInfected1 * (2 ** periodTypeMode()));
+const calc2 = Math.trunc(currentlyInfected2 * (2 ** periodTypeMode()));
 
 // Impact data calculations
 const impact = {
