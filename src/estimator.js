bdd7d8f1ac8fa5855feec91b18aea78cbff46b63
SimonKihara <kimcode254@gmail.com>
2020-04-13 14:01:56 +0300

    outputting the data object

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,23 @@
-const covid19ImpactEstimator = (data) => data;
+// This is the input data
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
 
+// All the data will be output in this function
+const covid19ImpactEstimator = () => {
+  const allData = {
+    data
+  };
+  return allData;
+};
 export default covid19ImpactEstimator;
