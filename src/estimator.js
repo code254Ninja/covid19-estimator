74464b7f035a3fa0455751c5e62774813cc86ef6
SimonKihara <kimcode254@gmail.com>
2020-04-13 14:48:56 +0300

    First challenge

--- src/estimator.js
+++ src/estimator.js
@@ -13,10 +13,41 @@ const data = {
   totalHospitalBeds: 1380614
 };
 
+// Function to output the date entry mode
+function periodTypeMode() {
+  let dateMode;
+  if (data.periodType === 'months') {
+    dateMode = Math.trunc((data.timeToElapse * 30) / 3);
+  } else if (data.periodType === 'weeks') {
+    dateMode = Math.trunc((data.timeToElapse * 7) / 3);
+  } else if (data.periodType === 'days') {
+    dateMode = Math.trunc(data.timeToElapse / 3);
+  }
+  return dateMode;
+}
+
+// Calculation for infected by the requested time
+const calc = Math.trunc(2 ** periodTypeMode());
+
+// Impact data calculations
+const impact = {
+  currentlyInfected: data.reportedCases * 10,
+  infectedByRequestedTime: this.currentlyInfected * calc
+
+};
+
+// SevereImpact Data Output
+const severeImpact = {
+  currentlyInfected: data.reportedCases * 50,
+  infectedByRequestedTime: this.currentlyInfected * calc
+
+};
 // All the data will be output in this function
 const covid19ImpactEstimator = () => {
   const allData = {
-    data
+    data,
+    impact,
+    severeImpact
   };
   return allData;
 };
