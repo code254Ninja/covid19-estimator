9b9d6dfcc98851cd71c71eef425add825af411a1
SimonKihara <kimcode254@gmail.com>
2020-04-14 23:20:07 +0300

    Think Phase

--- src/estimator.js
+++ src/estimator.js
@@ -12,59 +12,49 @@ const data = {
   population: 66622705,
   totalHospitalBeds: 1380614
 };
+const impact = {};
+const severeImpact = {};
+
+const { reportedCases, periodType, timeToElapse } = data;
+// Currently infected
+impact.currentlyInfected = reportedCases * 10;
+severeImpact.currentlyInfected = reportedCases * 50;
+
+// Function to output the date entry mode
+
+
+let dateMode;
+switch (periodType.toLowerCase()) {
+  case 'days':
+    dateMode = timeToElapse;
+    break;
+  case 'weeks':
+    dateMode = timeToElapse * 7;
+    break;
+  case 'month':
+    dateMode = timeToElapse * 30;
+    break;
+  default:
+    dateMode = timeToElapse;
+}
+
+
+impact.infectedByRequestedTime = Math.trunc(reportedCases * 10 * (2 ** (dateMode / 3)));
+severeImpact.infectedByRequestedTime = Math.trunc(reportedCases * 50 * (2 ** (dateMode / 3)));
+
+
+// Calculation for infected by the requested ti
+
 
 // All the data will be output in this function
 const covid19ImpactEstimator = () => {
-  const { reportedCases } = data;
-  // Currently infected
-  const currentlyInfected1 = reportedCases * 10;
-  const currentlyInfected2 = reportedCases * 50;
-
-  // Function to output the date entry mode
-  function periodTypeMode(params) {
-    const { periodType, timeToElapse } = params;
-    let dateMode;
-    switch (periodType.toLowerCase()) {
-      case 'days':
-        dateMode = timeToElapse;
-        break;
-      case 'weeks':
-        dateMode = timeToElapse * 7;
-        break;
-      case 'month':
-        dateMode = timeToElapse * 30;
-        break;
-      default:
-        dateMode = timeToElapse;
-    }
-    return dateMode;
-  }
-
-  // Calculation for infected by the requested time
-  const calc1 = currentlyInfected1 * Math.trunc(((2 ** periodTypeMode()) / 3));
-  const calc2 = currentlyInfected2 * Math.trunc(((2 ** periodTypeMode()) / 3));
-
-  // Impact data calculations
-  const impact = {
-    currentlyInfected: currentlyInfected1,
-    infectedByRequestedTime: calc1
-
-  };
-
-  // SevereImpact Data Output
-  const severeImpact = {
-    currentlyInfected: currentlyInfected2,
-    infectedByRequestedTime: calc2
-
-  };
   const allData = {
     data,
-    estimate: {
-      impact,
-      severeImpact
-    }
+    impact,
+    severeImpact
   };
-
   return allData;
 };
+
+
 export default covid19ImpactEstimator;
